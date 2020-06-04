4c8bf4eb17e0e1be396cec78313a46d37c1b895d
awanabdulwahab7@outlook.com <awanabdulwahab7@outlook.com>
2020-06-04 14:16:55 +0500

    MatchupEntryModel class uploaded

---
+++ TrackerLibrary/MatchupEntryModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackerLibrary
+{
+    public class MatchupEntryModel
+    {
+        public List<TeamModel> TeamsCompeting { get; set; } = new List<TeamModel>();
+        public double Score { get; set; }
+        public List<MatchupModel> ParentMatchup { get; set; }
+    }
+}
