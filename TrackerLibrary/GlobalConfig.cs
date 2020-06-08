004a5409caea79f7da89eedb798226a1773f158c
awanabdulwahab7@outlook.com <awanabdulwahab7@outlook.com>
2020-06-08 14:37:21 +0500

    Golobal Connfiguration class uploaded successfully

---
+++ TrackerLibrary/GlobalConfig.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackerLibrary
+{
+    /// <summary>
+    /// This class contains the dataconnetion to store data in text file or sql server
+    /// </summary>
+    public static class GlobalConfig
+    {
+        /// <summary>
+        /// This list of data connection is used to save the data in both text file or sql server at the same time
+        /// </summary>
+
+        public static List<IDataConnection> Connections { get; private set; }
+        /// <summary>
+        /// At the begining of applicaiton, these are the connections I want to setup 
+        /// </summary>
+        /// <param name="database">
+        /// Do you want database 
+        /// </param>
+        /// <param name="textFiles">
+        /// Do you want the text file
+        /// </param>
+        public static void InitializeConnection(bool database, bool textFiles)
+        {
+            if (database)
+            {
+                // do something
+            }
+            if (textFiles)
+            {
+                // do something
+            }
+        }
+    }
+}
