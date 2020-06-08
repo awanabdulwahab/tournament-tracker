ebff2b8348aa2915797992e7384183527f3d18c9
awanabdulwahab7@outlook.com <awanabdulwahab7@outlook.com>
2020-06-08 16:00:35 +0500

    Validate Create Prize Form

--- TournamentTrackerForms/CreatePrizeForm.cs
+++ TournamentTrackerForms/CreatePrizeForm.cs
@@ -16,5 +16,54 @@ namespace TournamentTrackerForms
         {
             InitializeComponent();
         }
+
+        private void createPrizeButton_Click(object sender, EventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// Validate the form create Prize 
+        /// </summary>
+        /// <returns>
+        /// Returns true if all the information in form is valid
+        /// </returns>
+        private bool validateForm()
+        {
+            bool output = true;
+            int placenumber = 0;
+            bool placeNumberValidNumber = int.TryParse(placeNameValue.Text, out placenumber);
+            if (placeNumberValidNumber == false)
+            {
+                output = false;
+            }
+            if(placenumber < 1)
+            {
+                output = false;
+            }
+
+            if (placeNameValue.Text.Length == 0)
+            {
+                output = false;
+            }
+
+            decimal prizeamount = 0;
+            int prizepercentage = 0;
+
+            bool prizeAmountValid = decimal.TryParse(prizeAmountValue.Text, out prizeamount);
+            bool prizepercentageValid = int.TryParse(prizePercentageValue.Text, out prizepercentage);
+            if (prizeAmountValid == false || prizepercentageValid == false)
+            {
+                output = false;
+            }
+            if (prizeamount <=0 && prizepercentage <= 0)
+            {
+                output = false;
+            }
+            if (prizepercentage<0 || prizepercentage > 100)
+            {
+                output = false;
+            }
+            return output;
+        }
     }
 }
