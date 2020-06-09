d3cf5c6be1a0ac4fd72c25e095341c3de1423648
awanabdulwahab7@outlook.com <awanabdulwahab7@outlook.com>
2020-06-09 16:17:20 +0500

    Add connection string in app.config to store data in text file

---
+++ TrackerClassLibrary/DataAccess/TextConnectorProcessor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerClassLibrary.DataAccess.TextConnector
+{
+    public static class TextConnectorProcessor
+    {
+        public static string FullFillPath (string fileName) // PrizeModel.csv
+        {
+            // D:\UET\Summer\Windows Form Application\tournament tracker\Data\PrizeModel.csv
+            return $"{ConfigurationManager.AppSettings["filePath"]}\\{ fileName}";
+        }
+    }
+}
